7254fb3f447705ac509d1073ba7817c406dbf63d
teteretu <teteretu@yahoo.com>
2021-05-22 23:48:53 -0300

    implement menu and gameover scenes And Pause game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private bool paused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            Debug.Log("Calling");
+            if (paused)
+            {
+                paused = false;
+                ResumeGame();
+            } else
+            {
+                paused = true;
+                PauseGame();
+            }
+        }
+    }
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
---
+++ Assets/Scripts/StartController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class StartController : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene("Main");
+        }
+	}
+
+}
